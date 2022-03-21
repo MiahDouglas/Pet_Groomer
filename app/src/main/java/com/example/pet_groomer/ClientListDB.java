eada5bf85c5a4266b31aadde1223b060b4b8f444
MiahDouglas <98913999+MiahDouglas@users.noreply.github.com>
2022-03-21 18:54:46 -0400

    Update ClientListDB.java

--- app/src/main/java/com/example/pet_groomer/ClientListDB.java
+++ app/src/main/java/com/example/pet_groomer/ClientListDB.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class ClientListDB extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "FeedReader.db";
+    public static final String DATABASE_NAME = "PetGroomer.db";
 
     public ClientListDB(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
